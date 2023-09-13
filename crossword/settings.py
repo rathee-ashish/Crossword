5b12e4dbb8f0fac8c055c68e27ac71948d19518a
Ashish <138308243+rathee-ashish@users.noreply.github.com>
2023-09-13 23:47:17 +0530

    Update settings.py

--- crossword/settings.py
+++ crossword/settings.py
@@ -138,6 +138,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS =True
 
-import os
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
