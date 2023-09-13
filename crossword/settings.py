a1a8b385ce3761721c7cec19bea2dfa44dfecdb0
Ashish <ashishrathee18922@gmail.com>
2023-09-13 23:14:00 +0530

    first

--- crossword/settings.py
+++ crossword/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*f+vp@h*bt8mbgzpul_&ma5u4-7)!2^prj56$8scl4y9$+1-3&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vervel.app']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -137,3 +137,7 @@ STATICFILES_DIRS=[
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS =True
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
