5457e5f00b397a5c0b83d7a6fe0ce72a8203523c
Ashish <138308243+rathee-ashish@users.noreply.github.com>
2023-09-14 00:03:27 +0530

    Update settings.py

--- crossword/settings.py
+++ crossword/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*f+vp@h*bt8mbgzpul_&ma5u4-7)!2^prj56$8scl4y9$+1-3&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
